5fb4cc41be6e40e8505c3718f60d6d7b8db71f21
nbarnes2021 <95229371+nbarnes2021@users.noreply.github.com>
2023-09-25 21:15:36 -0400

    Delete MarketBasketAnalysis directory

--- MarketBasketAnalysis/SQLScripts/01_initial_data_exploration.sql
+++
@@ -1,8 +0,0 @@
--- Script: 01_initial_data_exploration.sql
--- Purpose: Perform initial data exploration and validation queries.
-
--- Count total rows in the table
-SELECT COUNT(*) FROM market_basket_data;
-
--- Display sample records
-SELECT * FROM market_basket_data LIMIT 10;
--- MarketBasketAnalysis/SQLScripts/05_data_exportation.sql
+++
@@ -1,5 +0,0 @@
--- Script: 05_data_exportation.sql
--- Purpose: Export clean data into csv file
-
-copy market_basket_data to 'C:\Users\14043\Documents\DataAnalysisProjects\DataInsightsPortfolio\MarketBasketAnalys
-is\Data\OnlineRetailCleansed.csv' with csv header;
